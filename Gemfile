c22003ae03fd5bd1c9495ec4710d8ec8bb3f7dfd
chinweokwu <paulcmorah@gmail.com>
2022-07-05 20:35:23 +0100

    add cart and order logic

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ group :development do
 end
 
 gem "avo"
+
+gem "tailwindcss-rails", "~> 2.0"
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+  def total
+    product.price * quantity
+  end
 end
