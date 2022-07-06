5c5ab0db4ec2a1b38264e41e5d7feddd059830bc
chinweokwu <paulcmorah@gmail.com>
2022-07-06 03:37:46 +0100

    fix failing styles

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :orders
   has_many :carts, through: :orders
+  def shrink_description
+    ActionView::Base.full_sanitizer.sanitize(description).truncate 50
+  end
 end
